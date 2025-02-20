8a88d2a2dcc461e78c80800b4facc508605dd8a3
Constantin <paladuta05@gmail.com>
2025-02-20 16:37:39 +0200

    ref

--- src/pages/api/stripe/webhook.js
+++ src/pages/api/stripe/webhook.js
@@ -35,8 +35,8 @@ export default async function handler(req, res) {
       await stripe.paymentIntents.update(session.payment_intent, {
         metadata: {
           // payment_link_id: session.metadata?.payment_link_id || "plink_1QtnQ7Eop8dXaHk5KT3KWoFA",
+          // payment_link_id: session.metadata?.payment_link_id || "plink_1QtXZhHWwCgaMkWCzM9cDLUa",
           payment_link_id: session.metadata?.payment_link_id || "plink_1QtXZhHWwCgaMkWCzM9cDLUa",
-          // payment_link_id: session.metadata?.payment_link_id || "plink_1Qtn9lGgA3LFaOpfZ42zoNJo",
 
 
         },
